42e6d601022ee5c6a91e6eebf7b96f7e84df5a68
张奇亚 <1137940230@qq.com>
2020-12-18 16:33:09 +0800

    功能优化-统一处理页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { Dialog } from 'vant'
+import store from '@/store/'
+
+// 网上查的 解决路由版本问题
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalPush.call(this, location, onResolve, onReject)
+  }
+  return originalPush.call(this, location).catch(err => err)
+}
 
 Vue.use(VueRouter)
 
@@ -8,7 +19,9 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/login/index.vue')
+    component: () => import('@/views/login/index.vue'),
+    // 添加额外的自定义数据
+    meta: { requiresAuth: false }
   },
   {
     path: '/',
@@ -20,51 +33,96 @@ const routes = [
         // 默认子路由,只能有一个
         path: '',
         name: 'home',
-        component: () => import('@/views/home/')
+        component: () => import('@/views/home/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/qa',
         name: 'qa',
-        component: () => import('@/views/qa/')
+        component: () => import('@/views/qa/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/video',
         name: 'video',
-        component: () => import('@/views/video/')
+        component: () => import('@/views/video/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/my',
         name: 'my',
-        component: () => import('@/views/my/')
+        component: () => import('@/views/my/'),
+        meta: { requiresAuth: false }
       }
     ]
   },
   {
     path: '/search',
     name: 'search',
-    component: () => import('@/views/search')
+    component: () => import('@/views/search'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/article/:articleId',
     name: 'article',
     component: () => import('@/views/article'),
     // 开启 props 传参，说白了就是把路由参数映射到组件的 props 数据中
-    props: true
+    props: true,
+    meta: { requiresAuth: false }
   },
   {
     path: '/user/profile',
     name: 'user-profile',
-    component: () => import('@/views/user-profile')
+    component: () => import('@/views/user-profile'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/user/chat',
     name: 'user-chat',
-    component: () => import('@/views/user-chat')
+    component: () => import('@/views/user-chat'),
+    meta: { requiresAuth: true }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 导航守卫
+// to: 要访问的页面路由信息
+// from: 来自哪个页面的路由信息
+// next: 放行的标记
+router.beforeEach((to, from, next) => {
+  // 判断页面是否需要登录才能访问
+  if (to.meta.requiresAuth) {
+    // 如果已登录 则直接通过
+    if (store.state.user) {
+      return next()
+    }
+
+    // 没有登录 提示是否登录
+    Dialog.confirm({
+      title: '访问提示',
+      message: '该功能需要登录才能访问，确定登录吗?'
+    })
+      .then(() => {
+        // 确认执行这里
+        router.replace({
+          name: 'login',
+          query: {
+            redirect: router.currentRoute.fullPath
+          }
+        })
+      })
+      .catch(() => {
+        // 取消执行这里
+        // 取消了 停止路由导航
+        next(false)
+      })
+  } else {
+    // 不需要登录状态的页面 直接过去
+    next()
+  }
 })
 
 export default router
